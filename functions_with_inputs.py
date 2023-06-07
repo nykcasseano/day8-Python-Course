1cb1b7374e3f6e5fd044f3cbff309d417445a89f
nyk <nykcasseano13@gmail.com>
2023-06-06 22:33:09 -0300

    último commit

---
+++ functions_with_inputs.py
@@ -0,0 +1,10 @@
+#Simple Function
+# def greet():
+# print ("Hello Nyk")
+# greet()
+#Function that allows for input
+
+def greet_with_name(name):
+    print(f"Hello {name}")
+    print(f"How do you do {name}")
+greet_with_name("Nyk")     
